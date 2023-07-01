c451eca7af0a54ae9e72956dd904a260e266ced4
softyAmir <theyabcpssouaf2@gmail.com>
2023-07-01 11:54:55 -1100

    third commit

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,16 +1,19 @@
-<a href="<?php echo get_permalink(); ?>">
-    <div class="card-items">
-        <div class="img-holder">
-            <img src="<?php echo the_post_thumbnail_url(); ?>" alt="featured image">
-        </div>
-        <div class="info">
-            <p><?php echo relevanssi_the_title(); ?></p>
-            <div class="hide">
-                <p> <?php echo substr(get_the_content(), 0, 100); ?>
-                </p>
-                <span><?php echo get_the_category()[0]->name; ?></span>
-            </div>
-            <span>workshhets</span>
-        </div>
-    </div>
-</a>             
+                <a href="<?php echo get_permalink(); ?>">
+                    <div class="card-items">
+                        <div class="img-holder">
+                            <img src="<?php echo the_post_thumbnail_url(); ?>" alt="featured image">
+                        </div>
+                        <div class="info">
+                            <p><?php echo relevanssi_the_title(); ?></p>
+                            <span>workshhets</span>
+                        </div>
+                        <div class="hide">
+                            <p><?php echo relevanssi_the_title(); ?></p>
+                            <p><?php echo substr(get_the_content(), 0, 100); ?></p>
+                            <span><?php echo get_the_category()[0]->name; ?></span>
+                            <span>workshhets</span>
+                        </div>
+                    </div>
+                </a>             
+
+                  
